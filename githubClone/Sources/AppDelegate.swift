c97cb1113fb286c9c8d9a324c9951c907c520042
dbsghdz1 <dbsghdz1@gmail.com>
2024-12-15 06:22:14 +0900

    App, Scene Deleate root 변경

--- githubClone/Sources/AppDelegate.swift
+++ githubClone/Sources/AppDelegate.swift
@@ -1,5 +1,24 @@
+//
+//  AppDelegate.swift
+//  githubClone
+//
+//  Created by 김윤홍 on 12/15/24.
+//
+
 import UIKit
- 
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = LoginViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
 }
--- githubClone/Sources/SceneDelegate.swift
+++ githubClone/Sources/SceneDelegate.swift
@@ -4,12 +4,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
  
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .yellow
+        let viewController = LoginViewController()
         
         self.window = window
         window.rootViewController = viewController
