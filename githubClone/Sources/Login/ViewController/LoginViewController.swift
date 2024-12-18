f052092ab31be7d94cbdc7969cf224cf31df6453
dbsghdz1 <dbsghdz1@gmail.com>
2024-12-18 22:01:18 +0900

    view 그리기

--- githubClone/Sources/Login/ViewController/LoginViewController.swift
+++ githubClone/Sources/Login/ViewController/LoginViewController.swift
@@ -6,32 +6,72 @@
 //
 
 import UIKit
-import PinLayout
-import FlexLayout
+
 import RxCocoa
+import RxSwift
 import Kingfisher
+import SnapKit
+import Then
 
 final class LoginViewController: UIViewController {
-    private let customView = CustomView()
     
-    override func loadView() {
-            // Set customView as the view of the controller
-            self.view = customView
+    private let disposeBag = DisposeBag()
+    
+    private lazy var loginButton = UIButton().then { button in
+        button.tintColor = .black
+        button.layer.cornerRadius = 30
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.setTitle("Github.com에 로그인", for: .normal)
+        button.backgroundColor = .black
+    }
+    
+    private lazy var loginDescription = UILabel().then { label in
+        label.text = "계정을 사용하여 GitHub.com에 로그인"
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 14)
+    }
+    
+    private lazy var gitHubLogoImage = UIImageView().then { imageView in
+        imageView.image = UIImage(named: "github-mark")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    func configureUI() {
+        self.view.backgroundColor = .systemBackground
+        
+        [
+            self.gitHubLogoImage,
+            self.loginButton,
+            self.loginDescription
+        ].forEach { self.view.addSubview($0) }
+        
+        gitHubLogoImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(200)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(75)
         }
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-
-            // Additional setup
-            customView.backgroundColor = .white
-
-            // Example: Adding action to the button
-            if let button = customView.subviews.first(where: { $0 is UIButton }) as? UIButton {
-                button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-            }
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(gitHubLogoImage.snp.bottom).offset(250)
+            make.horizontalEdges.equalToSuperview().inset(30)
+            make.height.equalTo(60)
         }
-
-        @objc private func buttonTapped() {
-            print("Button was tapped!")
+        
+        loginDescription.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(10)
+            make.centerX.equalTo(loginButton)
         }
+    }
+    
+    
+
+}
+
+#Preview {
+    LoginViewController()
 }
