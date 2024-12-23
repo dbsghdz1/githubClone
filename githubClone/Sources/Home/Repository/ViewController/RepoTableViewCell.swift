97cf15dbd79a9f0891d5722a78d06b06a3045a0b
dbsghdz1 <dbsghdz1@gmail.com>
2024-12-23 23:58:03 +0900

    TableViewCell추가

---
+++ githubClone/Sources/Home/Repository/ViewController/RepoTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  RepoTableViewCell.swift
+//  githubClone
+//
+//  Created by 김윤홍 on 12/23/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class RepoTableViewCell: UITableViewCell {
+    
+    private let nameLabel = UILabel().then { label in
+        label.text = "리포지토리"
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        contentView.addSubview(nameLabel)
+        
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(30)
+        }
+    }
+}
