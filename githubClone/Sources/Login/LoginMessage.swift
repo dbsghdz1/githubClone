36efae6fe8660eba9bc6eec88f816598bf7ae3c9
dbsghdz1 <dbsghdz1@gmail.com>
2025-01-01 22:01:46 +0900

    로그인 화면 메시지 따로 설정

---
+++ githubClone/Sources/Login/LoginMessage.swift
@@ -0,0 +1,11 @@
+//
+//  LoginMessage.swift
+//  githubClone
+//
+//  Created by 김윤홍 on 1/1/25.
+//
+
+enum loginMessage: String {
+    case loginButton = "Github.com에 로그인"
+    case loginDescription = "계정을 사용하여 GitHub.com에 로그인"
+}
