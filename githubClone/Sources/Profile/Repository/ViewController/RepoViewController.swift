67393ae8d011f8e036abe16d7b6d79b3847a4b35
Hong <dbsghdz1@gmail.com>
2025-03-22 12:34:08 +0900

    레포 뷰 스위프트 Ui 로 수정

--- githubClone/Sources/Profile/Repository/ViewController/RepoViewController.swift
+++ githubClone/Sources/Profile/Repository/ViewController/RepoViewController.swift
@@ -5,127 +5,120 @@
 //  Created by 김윤홍 on 12/22/24.
 //
 
-import UIKit
+import SwiftUI
 
-import RxCocoa
-import RxSwift
-import SnapKit
-import RxDataSources
-import Then
-
-//TODO: BaseVC 활용하기
-final class RepoViewController: UIViewController {
-    
-    private var disposeBag = DisposeBag()
-    private var dataSource: RxTableViewSectionedReloadDataSource<MySection>?
-    private let viewModel = RepoViewModel()
-    let didPop = BehaviorRelay<Bool>(value: false)
-    
-//    init(viewModel: RepoViewModel?) {
-//        self.viewModel = RepoViewModel()
-//        super.init(nibName: nil, bundle: nil)
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureUI()
-        bindDataSource()
-        bindUI()
-    }
-    
-    private let repoTableView = UITableView().then { tableView in
-        tableView.rowHeight = 100
-        tableView.register(RepoTableViewCell.self, forCellReuseIdentifier: String(describing: RepoTableViewCell.self))
-    }
-    
-    private let repoPlusButton = UIBarButtonItem().then { button in
-        button.image = UIImage(systemName: "plus")
-    }
-    
-    private func configureUI() {
-        
-        view.addSubview(repoTableView)
-        navigationItem.rightBarButtonItem = repoPlusButton
-        view.backgroundColor = .systemBackground
-        
-        repoTableView.snp.makeConstraints { make in
-            make.edges.equalTo(view.safeAreaLayoutGuide)
+struct RepoView: View {
+  @State private var repoData: [RepoModelElement] = RepoModel()
+  @State private var searchText = ""
+  @State private var ex = ["유형", "언어", "정렬: 최근에 푸시됨(내림차순)"]
+  
+  var body: some View {
+    VStack {
+      NavigationStack {
+        ChipLayout(verticalSpacing: 8, horizontalSpacing: 8) {
+          ForEach(ex.indices, id: \.self) { index in
+            let model = ex[index]
+            Text(model)
+              .padding(.horizontal, 12)
+              .padding(.vertical, 5)
+              .background(
+                Capsule().foregroundStyle(.gray)
+              )
+          }
         }
+        .padding(.leading, 16)
+        List(repoData) { item in
+          VStack(alignment: .leading) {
+            Text(item.name)
+              .fontWeight(.bold)
+            //              .padding(.init(top: 0.0, leading: 0.0, bottom: 3.0, trailing: 0.0))
+              .frame(alignment: .leading)
+            if item.description != nil {
+              Text(item.description ?? "")
+            }
+            HStack {
+              Image(systemName: "star")
+                .foregroundStyle(.gray)
+              Text("\(item.stargazersCount)")
+                .foregroundStyle(Color.gray)
+              //Swift 경우에만 색상 -- 수정 필요 --
+              Image(systemName: "circle.fill")
+                .foregroundStyle(.red)
+              Text(item.language?.rawValue ?? "")
+            }
+          }
+        }
+        .contentMargins(0, for: .scrollContent)
+        .navigationTitle("리포지토리")
+        .navigationBarTitleDisplayMode(.inline)
+      }
+      .searchable(
+        text: $searchText,
+        placement: .navigationBarDrawer(displayMode: .always),
+        prompt: "검색"
+      )
+      .onSubmit(of: .search) {
+        print(searchText)
+      }
     }
+    .onAppear {
+      Task {
+        do {
+          repoData = try await NetworkManager.shared.readRepo()
+        } catch {
+          print(error)
+        }
+      }
+    }
+  }
+}
+#Preview {
+  RepoView()
 }
 
-extension RepoViewController {
+struct ChipLayout: Layout {
+  
+  var verticalSpacing: CGFloat
+  var horizontalSpacing: CGFloat
+  
+  func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+    var totalHeigth: CGFloat = 0
+    var currentRowWidth: CGFloat = 0
+    var currentRowHeight: CGFloat = 0
     
-    private func bindDataSource() {
-        let dataSource = RxTableViewSectionedReloadDataSource<MySection>(configureCell: {
-            dataSource, tableView, indexPath, item in
-            guard let cell = tableView.dequeueReusableCell(
-                withIdentifier: String(describing: RepoTableViewCell.self),
-                for: indexPath
-            ) as? RepoTableViewCell else { return UITableViewCell() }
-
-            let repoTableModel = RepoTableModel(
-                repoName: item.name,
-                repoDescription: item.description
-            )
-            cell.configureUI(repoTableModel: repoTableModel)
-            return cell
-        })
-        self.dataSource = dataSource
+    for view in subviews {
+      let viewSize = view.sizeThatFits(.unspecified)
+      
+      if currentRowWidth + viewSize.width > (proposal.width ?? .infinity) {
+        totalHeigth += currentRowWidth + verticalSpacing
+        currentRowWidth = 0
+        currentRowHeight = 0
+      }
+      currentRowWidth += viewSize.width + horizontalSpacing
+      currentRowHeight = max(currentRowHeight, viewSize.height)
     }
     
-    private func bindUI() {
-        
-        let input = RepoViewModel.Input(
-//            viewWillAppearEvent: didPop,
-            viewDidLoadEvent: Observable.just(()),
-            deleteTapEvent: repoTableView.rx.itemDeleted,
-            repoPlusButtonTap: repoPlusButton.rx.tap,
-            repoTableCellTap: repoTableView.rx.itemSelected
-        )
-        let output =  viewModel.transform(input: input)
-        
-        //TODO: dataSource 저거 ! 처리고민해보쟈
-        //TODO: 
-//        output.viewWillAppear
-//            .drive(repoTableView.rx.items(dataSource: dataSource!))
-//            .disposed(by: disposeBag)
-        
-        output.repoData
-            .drive(repoTableView.rx.items(dataSource: dataSource!))
-            .disposed(by: disposeBag)
-
-        output.deleteData
-            .drive(onNext: { [weak self] in
-                guard let self else { return }
-                let alertMessage = AlertMessageModel(
-                    title: AlertMessage.deleteTitle.rawValue,
-                    message: AlertMessage.deleteMessage.rawValue,
-                    yesButtonTitle: AlertMessage.deleteOk.rawValue,
-                    cancelButtonTitle: nil,
-                    defaultButtonTitle: nil
-                )
-                showAlert(alertModel: alertMessage, Action: nil)
-            }).disposed(by: disposeBag)
-        
-        //고민: 모달띄우기, 네비전환 깔끔하게 처리할수 있을까? showAlert처럼 Ex활용해볼까/
-        output.repoPlusButtonTapped
-            .drive(onNext: { [weak self] in
-                guard let self else { return }
-                let naviModal = UINavigationController(rootViewController: RepoModalVC())
-                self.present(naviModal, animated: true)
-            }).disposed(by: disposeBag)
-        
-        output.repoTableCellTapped
-            .drive(onNext: { [weak self] repoData in
-                guard let self else { return }
-                let updateViewModel = UpdateRepoViewModel(repoData: BehaviorRelay(value: repoData))
-                let updateVC = UpdateRepoVC(viewModel: updateViewModel)
-                self.navigationController?.pushViewController(updateVC, animated: true)
-            }).disposed(by: disposeBag)
+    totalHeigth += currentRowHeight
+    return CGSize(width: proposal.width ?? 0, height: totalHeigth)
+  }
+  
+  func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+    var currentX: CGFloat = bounds.minX
+    var currentY: CGFloat = bounds.minY
+    var maxHeightInRow: CGFloat = 0
+    
+    for view in subviews {
+      let viewSize = view.sizeThatFits(.unspecified)
+      
+      if currentX + viewSize.width > bounds.maxX {
+        currentX = bounds.minX
+        currentY += maxHeightInRow + verticalSpacing
+        maxHeightInRow = 0
+      }
+      
+      view.place(at: CGPoint(x: currentX, y: currentY), anchor: .topLeading, proposal: .unspecified)
+      currentX += viewSize.width + horizontalSpacing
+      maxHeightInRow = max(maxHeightInRow, viewSize.height)
     }
+  }
 }
