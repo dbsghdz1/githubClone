bd9148e64c1800af001a1ead24970e9bf36223b2
Hong <dbsghdz1@gmail.com>
2025-03-30 18:25:30 +0900

    Tab바 삭제

--- githubClone/Sources/Common/Controller/TabBarController.swift
+++ githubClone/Sources/Common/Controller/TabBarController.swift
@@ -1,84 +1,80 @@
+////
+////  TabBarController.swift
+////  githubClone
+////
+////  Created by 김윤홍 on 12/22/24.
+////
 //
-//  TabBarController.swift
-//  githubClone
+//import UIKit
 //
-//  Created by 김윤홍 on 12/22/24.
+//final class TabBarController: UITabBarController {
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        viewConfig()
+//    }
+//    
+//    private func viewConfig() {
+//        let viewControllers = TabbarItem.allCases.map { item -> UINavigationController in
+//            let vc = item.viewController
+//            vc.view.backgroundColor = .systemBackground
+//            vc.navigationItem.title = item.navigtaionItemTitle
+//            vc.navigationItem.largeTitleDisplayMode = .always
+//            
+//            let nav = UINavigationController(rootViewController: vc)
+//            nav.title = item.navigtaionItemTitle
+//            nav.tabBarItem.image = item.tabbarImage
+//            nav.navigationBar.prefersLargeTitles = true
+//            
+//            return nav
+//        }
+//        setViewControllers(viewControllers, animated: false)
+//        tabBar.tintColor = UIColor.blue
+//    }
+//    
+//}
 //
-
-import UIKit
-
-final class TabBarController: UITabBarController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        viewConfig()
-    }
-    
-    private func viewConfig() {
-        let viewControllers = TabbarItem.allCases.map { item -> UINavigationController in
-            let vc = item.viewController
-            vc.view.backgroundColor = .systemBackground
-            vc.navigationItem.title = item.navigtaionItemTitle
-            vc.navigationItem.largeTitleDisplayMode = .always
-            
-            let nav = UINavigationController(rootViewController: vc)
-            nav.title = item.navigtaionItemTitle
-            nav.tabBarItem.image = item.tabbarImage
-            nav.navigationBar.prefersLargeTitles = true
-            
-            return nav
-        }
-        setViewControllers(viewControllers, animated: false)
-        tabBar.tintColor = UIColor.blue
-    }
-    
-}
-
-extension TabBarController {
-    
-    enum TabbarItem: CaseIterable {
-        case home
-        case noti
-        case search
-        case profile
-        
-        var viewController: UIViewController {
-            switch self {
-                case .home:
-                    return ViewController()
-                case .noti:
-                    return ViewController()
-                case .search:
-                    return ViewController()
-                case .profile:
-                    return RepoViewController()
-            }
-        }
-        
-        var tabbarImage: UIImage? {
-            switch self {
-                case .home:
-                    return UIImage(systemName: "house.fill")
-                case .noti:
-                    return UIImage(systemName: "house.fill")
-                case .search:
-                    return UIImage(systemName: "house.fill")
-                case .profile:
-                    return UIImage(systemName: "house.fill")
-            }
-        }
-        
-        var navigtaionItemTitle: String {
-            switch self {
-                case .home:
-                    return "홈"
-                case .noti:
-                    return "알림"
-                case .search:
-                    return "탐색"
-                case .profile:
-                    return "프로필"
-            }
-        }
-    }
-}
+//extension TabBarController {
+//    
+//    enum TabbarItem: CaseIterable {
+//        case home
+//        case noti
+//        case search
+//        case profile
+//        
+//        var viewController: UIViewController {
+//            switch self {
+//                case .profile:
+//                    return RepoViewController()
+//              default:
+//                return RepoViewController()
+//            }
+//        }
+//        
+//        var tabbarImage: UIImage? {
+//            switch self {
+//                case .home:
+//                    return UIImage(systemName: "house.fill")
+//                case .noti:
+//                    return UIImage(systemName: "house.fill")
+//                case .search:
+//                    return UIImage(systemName: "house.fill")
+//                case .profile:
+//                    return UIImage(systemName: "house.fill")
+//            }
+//        }
+//        
+//        var navigtaionItemTitle: String {
+//            switch self {
+//                case .home:
+//                    return "홈"
+//                case .noti:
+//                    return "알림"
+//                case .search:
+//                    return "탐색"
+//                case .profile:
+//                    return "프로필"
+//            }
+//        }
+//    }
+//}
